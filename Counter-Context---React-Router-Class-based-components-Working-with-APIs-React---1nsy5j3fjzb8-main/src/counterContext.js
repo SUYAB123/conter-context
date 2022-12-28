67db1db0a8b024d44cac06b8f1c57543a88fbf94
SUYAB123 <111383970+SUYAB123@users.noreply.github.com>
2022-12-29 01:45:54 +0530

    Add files via upload

---
+++ Counter-Context---React-Router-Class-based-components-Working-with-APIs-React---1nsy5j3fjzb8-main/src/counterContext.js
@@ -0,0 +1,24 @@
+import React, {useState, createContext } from "react";
+
+
+
+
+
+const counterContext = createContext()
+
+
+const CounterStateContext = (props) => {
+const [count,setCount]=useState(0);
+
+    return (
+        <div id="counter-context">
+            <counterContext.Provider value={{ count, setCount }}>
+                {props.children}
+            </counterContext.Provider >
+        </div>
+
+    )
+}
+
+export { counterContext, CounterStateContext }
+
